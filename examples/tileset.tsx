e5498e14199537afb956184ebe7164c254b2a827
Tom Mitchell <tom.0505@hotmail.co.uk>
2021-03-28 12:59:25 +0100

    Re-write example to demonstrate tileset usage

---
+++ examples/tileset.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="tileset" tilewidth="16" tileheight="16" tilecount="189" columns="21">
+ <image source="overworld.png" width="336" height="144"/>
+ <tile id="0" type="grass-island-nw"/>
+ <tile id="1" type="grass-island-n"/>
+ <tile id="2" type="grass-island-ne"/>
+ <tile id="21" type="grass-island-w"/>
+ <tile id="22" type="grass-island-center"/>
+ <tile id="23" type="grass-island-e"/>
+ <tile id="42" type="grass-island-sw"/>
+ <tile id="43" type="grass-island-s"/>
+ <tile id="44" type="grass-island-se"/>
+ <tile id="70" type="water"/>
+</tileset>
